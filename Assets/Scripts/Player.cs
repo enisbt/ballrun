e396265a47461fd413d29ba70a694c2dcc1109b3
Enis B. Tuysuz <enisbtuysuz@gmail.com>
2021-09-03 07:39:12 +0300

    1.0 Dump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float constantPush = 0.1f;
     [SerializeField] private float forceSpeedLimit = 50f;
 
+    private bool lockedVertical = false;
+    
     private Rigidbody rigidbody;
     private SceneManager sceneManager;
 
@@ -16,15 +18,20 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (!lockedVertical && rigidbody.velocity.magnitude > 4)
+        {
+            // Sometimes a ghost collision happens where two adjacent road fragment connects. To cancel that
+            // I am adding y position constraint to rigidbody.
+            // Examples: https://forum.unity.com/threads/how-to-prevent-ghost-collisions-without-dots.906839/
+            //           https://forum.unity.com/threads/walking-on-meshcolliders-generate-incorrect-collisions.895754/
+            //           https://forum.unity.com/threads/ghost-collisions-on-adjacent-box-colliders.850951/
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            lockedVertical = true;
+        }
         if (rigidbody.velocity.magnitude < forceSpeedLimit)
         {
             rigidbody.AddForce(-Vector3.forward * constantPush * Time.deltaTime);
         }
-    }
-
-    public float GetTopSpeed()
-    {
-        return forceSpeedLimit;
     }
 
     public float GetSpeed()
@@ -34,7 +41,7 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Obstacle")
+        if (other.gameObject.tag == "Obstacle" || other.gameObject.tag == "Side Wall")
         {
             sceneManager.GameOver();
         }
